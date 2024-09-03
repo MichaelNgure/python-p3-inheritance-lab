0b1946d56cee069357ea7dd07ba44ed33eb8ed8d
MichaelNgure <mikynjoro01@gmail.com>
2024-09-03 22:16:13 +0300

    inheritance

--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,22 @@ from user import User
 import random
 
 class Teacher(User):
-
+    def __init__(self, first_name, last_name):
+    # Call super() / parent constructor
+        super().__init__(first_name, last_name)
+    # initialize knowledge
+        self.knowledge = [
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+]
     def teach(self):
-        pass
+        if len(self.knowledge) > 0:
+            randomIndex = random.randint(0, len(self.knowledge))
+            randomElement = self.knowledge[randomIndex]
+            return randomElement
